3a22908467c377c84b084141ce3f564c1a63f206
Rony Olivares <oh22008@ues.edu.sv>
2024-09-13 22:44:27 -0600

    Añadir carpeta TrabajoInvestigaPRN115_G5_2024 con el archivo TrabajoInvestigaPRN115_G5_2024.sln

---
+++ TrabajoInvestigaPRN115_G5_2024/Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace TrabajoInvestigaPRN115_G5_2024
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblCalculadora = new System.Windows.Forms.Label();
+            this.lblNumero1 = new System.Windows.Forms.Label();
+            this.lblNumero2 = new System.Windows.Forms.Label();
+            this.lblOperacion = new System.Windows.Forms.Label();
+            this.lblOperaciones = new System.Windows.Forms.Label();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.txtNumero1 = new System.Windows.Forms.TextBox();
+            this.txtResultado = new System.Windows.Forms.TextBox();
+            this.txtNumero2 = new System.Windows.Forms.TextBox();
+            this.CBoxOperaciones = new System.Windows.Forms.ComboBox();
+            this.btnEjecutar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblCalculadora
+            // 
+            this.lblCalculadora.AutoSize = true;
+            this.lblCalculadora.Font = new System.Drawing.Font("Roboto", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCalculadora.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.lblCalculadora.Location = new System.Drawing.Point(231, 21);
+            this.lblCalculadora.Name = "lblCalculadora";
+            this.lblCalculadora.Size = new System.Drawing.Size(180, 35);
+            this.lblCalculadora.TabIndex = 0;
+            this.lblCalculadora.Text = "Calculadora";
+            // 
+            // lblNumero1
+            // 
+            this.lblNumero1.AutoSize = true;
+            this.lblNumero1.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero1.Location = new System.Drawing.Point(54, 138);
+            this.lblNumero1.Name = "lblNumero1";
+            this.lblNumero1.Size = new System.Drawing.Size(170, 22);
+            this.lblNumero1.TabIndex = 1;
+            this.lblNumero1.Text = "Coloque el numero:";
+            // 
+            // lblNumero2
+            // 
+            this.lblNumero2.AutoSize = true;
+            this.lblNumero2.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero2.Location = new System.Drawing.Point(54, 173);
+            this.lblNumero2.Name = "lblNumero2";
+            this.lblNumero2.Size = new System.Drawing.Size(170, 22);
+            this.lblNumero2.TabIndex = 2;
+            this.lblNumero2.Text = "Coloque el numero:";
+            // 
+            // lblOperacion
+            // 
+            this.lblOperacion.AutoSize = true;
+            this.lblOperacion.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperacion.Location = new System.Drawing.Point(355, 154);
+            this.lblOperacion.Name = "lblOperacion";
+            this.lblOperacion.Size = new System.Drawing.Size(148, 22);
+            this.lblOperacion.TabIndex = 3;
+            this.lblOperacion.Text = "Elija la operación";
+            // 
+            // lblOperaciones
+            // 
+            this.lblOperaciones.AutoSize = true;
+            this.lblOperaciones.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperaciones.Location = new System.Drawing.Point(21, 98);
+            this.lblOperaciones.Name = "lblOperaciones";
+            this.lblOperaciones.Size = new System.Drawing.Size(222, 21);
+            this.lblOperaciones.TabIndex = 4;
+            this.lblOperaciones.Text = "OPERACIONES A REALIZAR:";
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft YaHei UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.ForeColor = System.Drawing.Color.Red;
+            this.lblResultado.Location = new System.Drawing.Point(273, 215);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(103, 22);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Resultado=";
+            // 
+            // txtNumero1
+            // 
+            this.txtNumero1.BackColor = System.Drawing.Color.Moccasin;
+            this.txtNumero1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNumero1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero1.Location = new System.Drawing.Point(230, 138);
+            this.txtNumero1.Name = "txtNumero1";
+            this.txtNumero1.Size = new System.Drawing.Size(119, 26);
+            this.txtNumero1.TabIndex = 6;
+            // 
+            // txtResultado
+            // 
+            this.txtResultado.BackColor = System.Drawing.Color.Cornsilk;
+            this.txtResultado.Cursor = System.Windows.Forms.Cursors.No;
+            this.txtResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResultado.Location = new System.Drawing.Point(228, 240);
+            this.txtResultado.Name = "txtResultado";
+            this.txtResultado.Size = new System.Drawing.Size(198, 26);
+            this.txtResultado.TabIndex = 7;
+            this.txtResultado.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // txtNumero2
+            // 
+            this.txtNumero2.BackColor = System.Drawing.Color.Moccasin;
+            this.txtNumero2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtNumero2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumero2.Location = new System.Drawing.Point(229, 173);
+            this.txtNumero2.Name = "txtNumero2";
+            this.txtNumero2.Size = new System.Drawing.Size(122, 26);
+            this.txtNumero2.TabIndex = 7;
+            // 
+            // CBoxOperaciones
+            // 
+            this.CBoxOperaciones.BackColor = System.Drawing.Color.Moccasin;
+            this.CBoxOperaciones.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.CBoxOperaciones.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.CBoxOperaciones.FormattingEnabled = true;
+            this.CBoxOperaciones.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.CBoxOperaciones.Location = new System.Drawing.Point(504, 152);
+            this.CBoxOperaciones.Name = "CBoxOperaciones";
+            this.CBoxOperaciones.Size = new System.Drawing.Size(121, 28);
+            this.CBoxOperaciones.TabIndex = 8;
+            this.CBoxOperaciones.SelectedIndexChanged += new System.EventHandler(this.CBoxOperaciones_SelectedIndexChanged);
+            // 
+            // btnEjecutar
+            // 
+            this.btnEjecutar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnEjecutar.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEjecutar.Location = new System.Drawing.Point(190, 300);
+            this.btnEjecutar.Name = "btnEjecutar";
+            this.btnEjecutar.Size = new System.Drawing.Size(78, 28);
+            this.btnEjecutar.TabIndex = 9;
+            this.btnEjecutar.Text = "Ejecutar";
+            this.btnEjecutar.UseVisualStyleBackColor = true;
+            this.btnEjecutar.Click += new System.EventHandler(this.btnEjecutar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnLimpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnLimpiar.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpiar.Location = new System.Drawing.Point(285, 300);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(78, 28);
+            this.btnLimpiar.TabIndex = 10;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.btnCerrar.Font = new System.Drawing.Font("Microsoft YaHei UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCerrar.Location = new System.Drawing.Point(383, 300);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(78, 28);
+            this.btnCerrar.TabIndex = 11;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(637, 366);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnEjecutar);
+            this.Controls.Add(this.CBoxOperaciones);
+            this.Controls.Add(this.txtNumero2);
+            this.Controls.Add(this.txtResultado);
+            this.Controls.Add(this.txtNumero1);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.lblOperaciones);
+            this.Controls.Add(this.lblOperacion);
+            this.Controls.Add(this.lblNumero2);
+            this.Controls.Add(this.lblNumero1);
+            this.Controls.Add(this.lblCalculadora);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblCalculadora;
+        private System.Windows.Forms.Label lblNumero1;
+        private System.Windows.Forms.Label lblNumero2;
+        private System.Windows.Forms.Label lblOperacion;
+        private System.Windows.Forms.Label lblOperaciones;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.TextBox txtNumero1;
+        private System.Windows.Forms.TextBox txtResultado;
+        private System.Windows.Forms.TextBox txtNumero2;
+        private System.Windows.Forms.ComboBox CBoxOperaciones;
+        private System.Windows.Forms.Button btnEjecutar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+    }
+}
+
---
+++ TrabajoInvestigaPRN115_G5_2024/Form1.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrabajoInvestigaPRN115_G5_2024
+{
+    public partial class Form1 : Form
+    {
+        /// <summary>
+        /// Constructor del formulario. Inicializa los componentes y agrega las operaciones al ComboBox.
+        /// También establece el campo de resultado como de solo lectura.
+        /// </summary>
+        public Form1()
+        {
+            //agrega al combo box los items a utilizar
+            InitializeComponent();
+            CBoxOperaciones.Items.Add("Sumar");
+            CBoxOperaciones.Items.Add("Restar");
+            CBoxOperaciones.Items.Add("Multiplicar");
+            CBoxOperaciones.Items.Add("Dividir");
+
+            //hace que el TextBox sea solo lectura
+            txtResultado.ReadOnly = true;
+        }
+        /// <summary>
+        /// Este método se ejecuta al hacer clic en el botón 'Ejecutar'.
+        /// Realiza la operación seleccionada (sumar, restar, multiplicar o dividir) sobre dos números ingresados.
+        /// </summary>
+       
+        /// <remarks>
+        /// Si se selecciona "Dividir", se verifica que el segundo número no sea 0 para evitar divisiones entre 0.
+        /// Si los valores ingresados no son números válidos, se muestra un mensaje de advertencia.
+        /// </remarks>
+        private void btnEjecutar_Click(object sender, EventArgs e)
+        {
+            double numero1, numero2, resultado = 0;
+
+            // Intentar convertir los valores de los TextBox en números.
+            if (double.TryParse(txtNumero1.Text, out numero1) && double.TryParse(txtNumero2.Text, out numero2))
+            {
+                string operacionSeleccionada = CBoxOperaciones.SelectedItem?.ToString(); // Manejo seguro de la selección.
+
+                // Evaluar la operación seleccionada.
+                switch (operacionSeleccionada)
+                {
+                    case "Sumar":
+                        resultado = numero1 + numero2;
+                        break;
+                    case "Restar":
+                        resultado = numero1 - numero2;
+                        break;
+                    case "Multiplicar":
+                        resultado = numero1 * numero2;
+                        break;
+                    case "Dividir":
+                        if (numero2 != 0) // Verificar que no se divida entre 0
+                        {
+                            resultado = numero1 / numero2;
+                        }
+                        else
+                        {
+                            MessageBox.Show("No se puede dividir entre 0.");
+                            return;
+                        }
+                        break;
+                    default:
+                        MessageBox.Show("Selecciona una operación válida.");
+                        return;
+                }
+
+                // Mostrar el resultado en el TextBox de resultado.
+                txtResultado.Text = resultado.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Introduce números válidos.");
+            }
+        }
+
+        private void CBoxOperaciones_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Limpia todos los campos de entrada (números y operación) al hacer clic en el botón 'Limpiar'.
+        /// </summary>
+
+        /// <remarks>
+        /// Restablece los campos `txtNumero1`, `txtNumero2` y `txtResultado`.
+        /// También elimina la selección del ComboBox de operaciones.
+        /// </remarks>
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            txtNumero1.Text = "";
+            txtNumero2.Text = "";
+            txtResultado.Text = "";
+            CBoxOperaciones.SelectedIndex = -1; // Desmarcar la selección del ComboBox
+        }
+        /// <summary>
+        /// Cierra el formulario cuando se presiona el botón 'Cerrar'.
+        /// </summary>
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            this.Close();   
+        }
+    }
+}
